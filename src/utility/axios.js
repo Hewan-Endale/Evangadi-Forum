7567fedf22df6b37be530677dd3f98ae90d993a4
Hewan-Endale <Hewanendale2@gmail.com>
2025-01-04 17:54:05 +0300

    edit

--- src/utility/axios.js
+++ src/utility/axios.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
-// used to if server is in use
-// const Ports = import.meta.env.PORT || 3004;
-const axiosInstance = axios.create({
-  
-  // deployed endpoint reference
-  baseURL: "http://localhost:3004/api/",
-});
 
-export { axiosInstance };
+   // used to if server is in use
+// const Ports = import.meta.env.PORT || 3004;
+
+export const axiosInstance = axios.create({
+  //local endpoint reference
+  // baseURL: `http://localhost:${Ports}/api`,
+
+  // deployed endpoint reference
+  baseURL: "https://ev-forum-backend.onrender.com",
+
+});
